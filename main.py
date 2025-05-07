2ceaea0aa4f14df87d8b1ad94ba2d87600291bf4
AlejandroAP98 <aalvarezpa@unal.edu.co>
2025-05-06 23:59:13 -0500

    Integrar flujo completo de ejecución en main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from estudiantes.registro import cargar_estudiantes, ordernar_estudiantes, mostrar_tabular, calcular_promedio
 
 def main():
+    #flujo principal de ejecutar el programa
     archivo = 'estudiantes.csv'
     estudiantes = cargar_estudiantes(archivo)
-
     estudiantes_ordenados = ordernar_estudiantes(estudiantes)
-
     promedios = calcular_promedio(estudiantes_ordenados)
-
     mostrar_tabular(promedios)
-
 
 if __name__ == '__main__':
     main()
